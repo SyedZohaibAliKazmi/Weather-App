a303388f7becefbb89a41dfe2ecd5afde071dce5
syedzohaibalikazmi <syedzohaibali9900@gmail.com>
2024-05-23 00:14:28 +0500

    App

---
+++ app.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("#myForm");
+const cityInput = document.querySelector("#cityInput");
+const API_KEY = "107f0a4900fe176149bf71880f2351a2";
+const temp = document.querySelector("#temp");
+const humidity = document.querySelector("#humidity");
+
+const formHandler = async (event) => {
+  event.preventDefault();
+
+  const city = cityInput.value;
+
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?${city}&appid=${API_KEY}&units=metric`
+  );
+  const data = await response.json();
+
+  temp.innerText = data.main.temp;
+  humidity.innerText = data.main.humidity;
+
+  console.log("🚀 ~ formHandler ~ response:", data);
+};
+
+form.addEventListener("submit", formHandler);
+
